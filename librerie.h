d68f7682a62ad797a4911d35cd10bc1141ed7026
gloriazen <gloria.zen@unitn.it>
2018-10-25 16:40:52 +0200

    Adding files

---
+++ librerie.h
@@ -0,0 +1,54 @@
+#ifndef __LIBRERIE__
+#define __LIBRERIE__
+
+#include <opencv/cv.h>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/core/utility.hpp>
+#include "opencv2/core/affine.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+
+////////////////////////////////////cuda
+#include <opencv2/core.hpp>
+#include <opencv2/core/cuda_types.hpp>
+#include <opencv2/core/cuda.inl.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/core/cuda_stream_accessor.hpp>
+#include <opencv2/cudalegacy/NCVHaarObjectDetection.hpp>
+#include <opencv2/cudalegacy/NCV.hpp>
+#include <opencv2/cudalegacy/NPP_staging.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/ocl_genbase.hpp>
+#include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudawarping.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <opencv2/cudafilters.hpp>
+//////////////////////////////////////
+
+
+#include <ctime>
+#include <cstdio>
+
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+
+//#include "load_param.h"
+//#include "CSVReader.h"
+//#include "load_pid.h"
+//#include "crop_img_glob.h"
+//#include "local_shift_calc.h"
+
+
+#endif
